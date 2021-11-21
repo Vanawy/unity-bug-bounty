c88a36ec18cd3112844d6d15ece524f6d4733a1a
Vanawy <saharchuk999@gmail.com>
2021-11-21 12:02:06 +0300

    Add intro cutscene

--- Assets/Scripts/IntroController.cs
+++ Assets/Scripts/IntroController.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IntroController : MonoBehaviour
 {
     [SerializeField]
     private WizardController _wizard;
+    [SerializeField]
+    private string _firstLevelScene;
 
     private void StartWizardFly()
     {
         _wizard.StartFlying();
+    }
+
+    private void NextScene()
+    {
+        SceneManager.LoadScene(_firstLevelScene);
     }
 }
